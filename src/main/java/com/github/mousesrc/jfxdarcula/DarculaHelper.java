a5fce068c0227cd92f126a23f725733da4e2517e
Mouse <937279156@qq.com>
2017-12-10 17:15:38 +0800

    Update

--- src/main/java/com/github/mousesrc/jfxdarcula/DarculaHelper.java
+++ src/main/java/com/github/mousesrc/jfxdarcula/DarculaHelper.java
@@ -6,10 +6,20 @@ import javafx.scene.Scene;
 public interface DarculaHelper {
 	
 	static String DARCULA_CONTAINER_CLASS = "darcula-container";
+	static String DARCULA_CONTAINER_DARK_CLASS = "darcula-container-dark";
+	static String DARCULA_CONTAINER_BRIGHT_CLASS = "darcula-container-bright";
 	static String DARCULA_CSS_URL = DarculaHelper.class.getResource("darcula.css").toExternalForm();
 
 	static void setDarculaContainerStyleClass(Parent parent){
 		parent.getStyleClass().add(DARCULA_CONTAINER_CLASS);
+	}
+	
+	static void setDarculaContainerStyleDarkClass(Parent parent){
+		parent.getStyleClass().add(DARCULA_CONTAINER_DARK_CLASS);
+	}
+	
+	static void setDarculaContainerStyleBrightClass(Parent parent){
+		parent.getStyleClass().add(DARCULA_CONTAINER_BRIGHT_CLASS);
 	}
 	
 	static void setDarculaCss(Scene scene){
