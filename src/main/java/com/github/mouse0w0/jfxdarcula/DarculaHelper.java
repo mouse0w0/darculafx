e7a6c1e5f7d7bf4a7b514bdded057c81a73c4699
Mouse <mouse0w0@qq.com>
2021-01-27 23:02:04 +0800

    Update DarculaHelper

--- src/main/java/com/github/mouse0w0/jfxdarcula/DarculaHelper.java
+++ src/main/java/com/github/mouse0w0/jfxdarcula/DarculaHelper.java
@@ -3,15 +3,19 @@ package com.github.mouse0w0.jfxdarcula;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 
-public interface DarculaHelper {
-	
-	static String DARCULA_CSS_URL = DarculaHelper.class.getResource("darcula.css").toExternalForm();
-	
-	static void applyDarculaStyle(Scene scene){
-		scene.getStylesheets().add(DARCULA_CSS_URL);
-	}
-	
-	static void applyDarculaStyle(Parent parent){
-		parent.getStylesheets().add(DARCULA_CSS_URL);
-	}
+public final class DarculaHelper {
+
+    public static final String DARCULA_STYLE_URL = DarculaHelper.class.getResource("darcula.css").toExternalForm();
+
+    public static void applyDarculaStyle(Scene scene) {
+        scene.getStylesheets().add(DARCULA_STYLE_URL);
+    }
+
+    public static void applyDarculaStyle(Parent parent) {
+        parent.getStylesheets().add(DARCULA_STYLE_URL);
+    }
+
+    private DarculaHelper() {
+        // Nothing to do.
+    }
 }
